26ddc7cc26d39349db19a6149bbd79e849319bb6
Tricky <riosandres294@gmail.com>
2025-08-03 12:34:13 -0500

    names

--- hello_world/ciao.py
+++ hello_world/ciao.py
@@ -25,3 +25,31 @@ print(you)
 
 print(id(i))
 print(id(you))
+
+print("Here starts the list")
+
+names = ["Andres", "Cosmos"]
+nombres = names
+print(id(names))
+print(id(nombres))
+
+names.append("Cosmos")
+print(id(names))
+print(id(nombres))
+print(names)
+print(nombres)
+
+print("Here is the new variable")
+名字 = names[:]
+print(id(names))
+print(id(名字))
+print(names)
+print(名字)
+
+del names[-1]
+print(id(names))
+print(id(名字))
+print(names)
+print(名字)
+
+
